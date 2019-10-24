17d857560d15b273a02873afe2d144696cd7b80a
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-10-24 16:38:57 -0500

    connection between APIs were successful

--- SpringBoot-https-self-signed-certificate/src/main/java/com/springboot/https/controllers/MathController.java
+++ SpringBoot-https-self-signed-certificate/src/main/java/com/springboot/https/controllers/MathController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.core.env.Environment;
 
+import javax.print.DocFlavor;
 import javax.servlet.ServletException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,14 +25,14 @@ public class MathController {
         return "Welcome!!. This is an API!";
     }
 
-    @GetMapping("/sum/{numberA}/{numberB}")
-    public String sumInteger(@PathVariable Integer numberA, @PathVariable Integer numberB) throws ServletException {
+    @GetMapping("/sum")
+    public String sumInteger(@RequestParam String numberA, @RequestParam String numberB) throws ServletException {
         try {
-            String channel = "https://localhost:8444/operation";
+            Integer a = Integer.parseInt(numberA); Integer b = Integer.parseInt(numberB);
+            String channel = "http://localhost:8444/operation";
             System.out.println("CHANNEL COMMUNICATION: "+channel);
             return mathServices.getForObject(new URI(channel+"/sum/"+numberA+"/"+numberB), String.class);
         } catch (URISyntaxException e) {
-            e.printStackTrace();
             throw new ServletException("An unexpected error has occurred. Please try again!");
         }
     }
